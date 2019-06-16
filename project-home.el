3974435233e899d51c8a15dc4f0ac574772e39a6
lunarwaterfox <lunarwaterfox@gmail.com>
2019-06-16 21:47:01 +0800

    add change home method

--- project-home.el
+++ project-home.el
@@ -9,24 +9,25 @@
 
 (defvar project-home-list nil)
 
-(defun project-home-query-home (&optional recent)
+(defun project-home-query-directory (&optional recent)
   "Query user to point home director."
   (read-directory-name "Setup home directory: " recent nil t))
 
 (defun project-home-dir-by-key (key)
   "Export project home dir by key."
   (let ((dir (alist-get key project-home-list)))
-    (unless dir
-      (let ((recent (alist-get key project-home-history)))
-        (setf dir (project-home-query-home recent)))
-      (let ((pair (cons key dir))
-            (history (assq-delete-all key project-home-history)))
-        (push pair project-home-list)
-        (customize-save-variable 'project-home-history (cons pair history))))
+    (if dir dir (project-home-query-home key))))
+
+
+(defun project-home-query-home (key)
+  "Query home dir of a project by key."
+  (let* ((recent (alist-get key project-home-history))
+         (dir (project-home-query-directory recent))
+         (pair (cons key dir))
+         (home-list (assq-delete-all key project-home-list))
+         (history-list (assq-delete-all key project-home-history)))
+    (setq project-home-list (cons pair home-list))
+    (customize-save-variable 'project-home-history (cons pair history-list))
     dir))
-
-
-
-
 
 (provide 'project-home)
